7ad3a30ba0f500b85d03e11a0fb9b6fd94de025b
Ramon Casagrande <ramoncasagrande@hotmail.com>
2023-11-05 17:21:24 -0300

    Refatorado LivroService

--- src/main/java/com/unesc/bibliotecabackend/service/LivroService.java
+++ src/main/java/com/unesc/bibliotecabackend/service/LivroService.java
@@ -2,12 +2,16 @@ package com.unesc.bibliotecabackend.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.unesc.bibliotecabackend.model.Livro;
+import com.unesc.bibliotecabackend.model.exception.ResourceNotFoundException;
 import com.unesc.bibliotecabackend.repository.LivroRepository;
+import com.unesc.bibliotecabackend.shared.LivroDTO;
 
 @Service
 public class LivroService {
@@ -19,8 +23,11 @@ public class LivroService {
      * Método para obter todos as livros
      * @return a lista de livros
      */
-    public List<Livro> obterTodos(){
-        return livroRepository.obterTodos();
+    public List<LivroDTO> obterTodos(){
+        List<Livro> livros = livroRepository.findAll();
+        return livros.stream()
+            .map(livro -> new ModelMapper().map(livro, LivroDTO.class))
+            .collect(Collectors.toList());
     }
 
     /**
@@ -28,8 +35,13 @@ public class LivroService {
      * @param id do livro procurado
      * @return o livro procurado
      */
-    public Optional<Livro> obterPorId(Integer id){
-        return livroRepository.obterPorId(id);
+    public Optional<LivroDTO> obterPorId(Integer id){
+        Optional<Livro> livro = livroRepository.findById(id);
+        if(livro.isEmpty()){
+            throw new ResourceNotFoundException("Livro inexistente");
+        }
+        LivroDTO livroDTO = new ModelMapper().map(livro.get(), LivroDTO.class);
+        return Optional.of(livroDTO);
     }
 
     /**
@@ -37,8 +49,13 @@ public class LivroService {
      * @param livro a ser adicionado
      * @return o livro adicionado
      */
-    public Livro adicionar(Livro livro){
-        return livroRepository.adicionar(livro);
+    public LivroDTO adicionar(LivroDTO livroDTO){
+        livroDTO.setId(null);
+        ModelMapper mapper = new ModelMapper();
+        Livro livro = mapper.map(livroDTO, Livro.class);
+        livro = livroRepository.save(livro);
+        livroDTO.setId(livro.getId());
+        return livroDTO;
     }
 
     /**
@@ -46,7 +63,12 @@ public class LivroService {
      * @param id do livro a ser removido
      */
     public void remover(Integer id){
-        livroRepository.remover(id);
+        Optional<Livro> livro = livroRepository.findById(id);
+        if(livro.isEmpty()){
+            throw new ResourceNotFoundException("Livro inexistente");
+        }
+
+        livroRepository.deleteById(id);
     }
 
     /**
@@ -55,9 +77,13 @@ public class LivroService {
      * @param livro a ser atualizado
      * @return o livro atualizado
      */
-    public Livro atualizar(Integer id, Livro livro){
+    public LivroDTO atualizar(Integer id, LivroDTO livroDTO){
         //regra de negócio
-        livro.setId(id);
-        return livroRepository.atualizar(livro);
+
+        livroDTO.setId(id);
+        ModelMapper mapper = new ModelMapper();
+        Livro livro = mapper.map(livroDTO, Livro.class);
+        livroRepository.save(livro);
+        return livroDTO;
     }
 }
